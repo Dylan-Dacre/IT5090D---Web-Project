5d3322a443f83d377871e9e178edaa4397a85959
Dylan Dacre <dylan.dacre@developersinstitute.co.nz>
2023-11-26 15:42:12 +1300

    Contact page done.

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -2,9 +2,24 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div className="contact">
-      <h1>Contact</h1>
-      <p>This is the contact page. It's a static page that anyone can view.</p>
+    <div className="contact-container">
+      <div className="contact-header">
+        <h1>Contact</h1>
+        <p className="contact-description">
+          This is the contact page. It's a static page that anyone can view.
+        </p>
+      </div>
+      <div className="contact-form">
+        <form>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" />
+          <label htmlFor="email">Email</label>
+          <input type="text" id="email" name="email" />
+          <label htmlFor="message">Message</label>
+          <textarea id="message" name="message" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
