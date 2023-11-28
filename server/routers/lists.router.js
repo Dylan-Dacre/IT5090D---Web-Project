0c229a750c5622eb011b8d8b747862003884bc44
Dylan Dacre <dylan.dacre@developersinstitute.co.nz>
2023-11-28 16:36:34 +1300

    Router updates.

--- server/routers/lists.router.js
+++ server/routers/lists.router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const getLists = require("../src/repository");
 const createList = require("../src/repository");
+const updateList = require("../src/repository");
+const deleteList = require("../src/repository");
 const { checkJwt } = require("../middleware/authorizationMiddleware");
 
 // Get all items
@@ -17,6 +19,19 @@ router.get("/", checkJwt, async (req, res, next) => {
   }
 });
 
+// Get an item by id
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const userId = req.auth.payload.sub;
+    const listId = req.params.id;
+    const list = await getList.getList(userId, listId);
+    return res.status(200).json(list);
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Post an item
 
 router.post("/", async (req, res, next) => {
@@ -26,6 +41,34 @@ router.post("/", async (req, res, next) => {
     const listBody = { ...body, userId };
     const list = await createList.createList(listBody);
     return res.status(201).json(list);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Edit an item
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const userId = req.auth.payload.sub;
+    const listId = req.params.id;
+    const body = req.body;
+    const listBody = { ...body, userId };
+    const list = await updateList.updateList(userId, listId, listBody);
+    return res.status(201).json(list);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Delete an item
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const userId = req.auth.payload.sub;
+    const listId = req.params.id;
+    const list = await deleteList.deleteList(userId, listId);
+    return res.status(200).json(list);
   } catch (err) {
     next(err);
   }
--- server/routers/notes.router.js
+++ server/routers/notes.router.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const getNotes = require("../src/repository");
 const createNote = require("../src/repository");
+const updateNote = require("../src/repository");
+const deleteNote = require("../src/repository");
 const { checkJwt } = require("../middleware/authorizationMiddleware");
 
 // Get all items
@@ -17,6 +19,19 @@ router.get("/", checkJwt, async (req, res, next) => {
   }
 });
 
+// Get an item by id
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const userId = req.auth.payload.sub;
+    const noteId = req.params.id;
+    const note = await getNote.getNote(userId, noteId);
+    return res.status(200).json(note);
+  } catch (err) {
+    next(err);
+  }
+});
+
 // Post an item
 
 router.post("/", async (req, res, next) => {
@@ -26,6 +41,34 @@ router.post("/", async (req, res, next) => {
     const noteBody = { ...body, userId };
     const note = await createNote.createNote(noteBody);
     return res.status(201).json(note);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Edit an item
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const userId = req.auth.payload.sub;
+    const noteId = req.params.id;
+    const body = req.body;
+    const noteBody = { ...body, userId };
+    const note = await updateNote.updateNote(userId, noteId, noteBody);
+    return res.status(201).json(note);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Delete an item
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const userId = req.auth.payload.sub;
+    const noteId = req.params.id;
+    const note = await deleteNote.deleteNote(userId, noteId);
+    return res.status(200).json(note);
   } catch (err) {
     next(err);
   }
